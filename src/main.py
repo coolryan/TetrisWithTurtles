482e4be75a2326d4bf287ec848d7296e7e6a099a
Ryan Setaruddin <coolry1998@gmail.com>
2024-02-19 18:02:43 -0500

    install virtual env called .venv with all the libs, installed requirements.txt, created src folder with main file and tetris_model file.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+	Filename: main.py
+	Author: Ryan Setaruddin
+	Purpose: To execute the program
+	Date: Feb, 19, 2024
+"""
+
+def main():
+	pass
+	
+if __name__ == '__main__':
+	main()
---
+++ src/tetris_model.py
@@ -0,0 +1,12 @@
+"""
+	Filename: tetris_model.py
+	Author: Ryan Setaruddin
+	purpose: To create a class called Tetris.
+	This will represents the game
+	Date: Feb, 19, 2024
+"""
+class Tetris(object):
+	"""docstring for Tetris"""
+	def __init__(self, arg):
+		super(Tetris, self).__init__()
+		self.arg = arg
