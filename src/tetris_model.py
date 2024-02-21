ada74c9f1ab0e7f4cbcdbfb61f3502746e7f871e
Ryan Setaruddin <coolry1998@gmail.com>
2024-02-21 17:43:47 -0500

    drawing individual block with differnent alphabets such as i, s l, z, t and more.

--- src/tetris_model.py
+++ src/tetris_model.py
@@ -1,11 +1,22 @@
 """
     Filename: tetris_model.py
     Author: Ryan Setaruddin
-    purpose: To create a class called Tetris and base class called Block
+    purpose: To design a game called Tetris
+    by each of classes that may need help with such a base class called
+    Block where it takes 4 paramters in constructor and one draw method
+    in which can not draw its self
+
+    Another base class called IBlock which is drived from Block class
+    must have draw method which takes 4 paramters
+
+    another base class called Tetris which takes 4 paramters in its constructors
+    and one draw method
     This will represents the game
     Date: Feb, 19, 2024
 """
 import turtle
+import time
+import random
 
 class Block:
     """docstring for Block"""
@@ -20,8 +31,20 @@ class Block:
 
 class IBlock(Block):
     """docstring for I_Block"""
-    def draw(self):
-        pass
+    def draw(self, x: int, y: int, color: str, speed: int):
+        wall = []
+        for i in range(2):
+            block = turtle.Turtle()
+            block.shape("square")
+            block.color("color")
+            block.penup()
+            wall.append(block)
+        wall[0].goto(x, y)
+        wall.stamp()
+        wall[1].goto(x, y)
+        wall.stamp()
+
+        return wall
 
 class Tetris:
     """docstring for Tetris"""
@@ -33,3 +56,5 @@ class Tetris:
 
     def draw(self):
         pass
+
+iblock = IBlock(20, 20 "lightblue", 1)
